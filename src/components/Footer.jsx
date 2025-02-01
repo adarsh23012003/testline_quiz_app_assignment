9c244a94a3e1de88da5de83817b6e5727c338b93
Adarsh Maurya <adarshmaurya2303@gmail.com>
2025-02-01 19:19:35 +0530

    Header component and footer component created...

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+"use client";
+import React, { useState } from "react";
+import { AiFillHeart } from "react-icons/ai";
+
+function Footer() {
+  const [like, setLike] = useState(false);
+
+  return (
+    <footer className="border-t fixed w-full  bottom-0 z-10">
+      <p className="text-center text-gray-500 p-5 fill-red-500 bg-slate-300">
+        Copyright ©2025 All rights reserved | This site is made with{" "}
+        <span onClick={() => setLike(!like)} className="cursor-pointer">
+          <AiFillHeart
+            className={`inline ${like ? "fill-red-500" : "fill-gray-500"}`}
+          />
+          <span> by </span>
+          <a
+            href="https://testline.in/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-500 hover:underline font-semibold"
+          >
+            Testline
+          </a>
+        </span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import testline from "../assets/logo.png";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav className="w-full sticky top-0 py-3 px-5 border-b bg-slate-300">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-1">
+          <Link to={"/"}>
+            <img src={testline} alt="Logo" className="h-10 w-28" />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
