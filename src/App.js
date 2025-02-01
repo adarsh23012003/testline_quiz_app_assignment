4aeaa5dcb62ffa54277958e68b2e8b5e61e867da
Adarsh Maurya <adarshmaurya2303@gmail.com>
2025-02-01 19:19:55 +0530

    react router dom setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
